ea7dd051acf71ff0d08f0ae45da64e4ff6e106f1
James Dowty <jamesdowty1@gmail.com>
2023-11-27 13:47:46 -0800

    Fixed component issues

--- Math168/main.py
+++ Math168/main.py
@@ -1,10 +1,9 @@
-import csv
 import networkx as nx
 from import_test import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-metrolink = transitSystem("../Metrolink GTFS")
+metrolink = transitSystem("../Amtrak GTFS")
 
 G = nx.Graph()
 positions = {}
@@ -12,7 +11,7 @@ label = {}
 
 for stop in metrolink.stops:
     G.add_node(metrolink.stops[stop].stop_id)
-    positions.update({stop: np.array([ float(metrolink.stops[stop].stop_lon), float(metrolink.stops[stop].stop_lat)])})
+    positions.update({stop: np.array([float(metrolink.stops[stop].stop_lon), float(metrolink.stops[stop].stop_lat)])})
     label.update({stop: metrolink.stops[stop].stop_name})
 
 for trip in metrolink.trips:
@@ -23,9 +22,19 @@ for trip in metrolink.trips:
         travel_time += (int(to_time[0]) - int(from_time[0]))*60
         travel_time += (int(to_time[1]) - int(from_time[1]))
         travel_time += (int(to_time[2]) - int(from_time[2]))/60
-        G.add_edge(metrolink.trips[trip].sequence[i-1].stop_id, metrolink.trips[trip].sequence[i].stop_id, time=travel_time)
-labels = nx.get_edge_attributes(G, 'time')
-nx.draw_networkx(G, pos=positions, labels=label, font_size=6)
-nx.draw_networkx_edge_labels(G, positions, edge_labels=labels, font_size=6)
-plt.show()
+        G.add_edge(metrolink.trips[trip].sequence[i-1].stop_id, metrolink.trips[trip].sequence[i].stop_id, weight=travel_time)
+edge_weights = nx.get_edge_attributes(G, 'weight')
 
+average_shortest_path = 0
+component_count = 0
+
+for C in (G.subgraph(c).copy() for c in nx.connected_components(G)):
+    average_shortest_path += nx.average_shortest_path_length(C, weight='weight')
+    component_count += 1
+average_shortest_path = average_shortest_path/component_count
+
+print(average_shortest_path)
+print(component_count)
+nx.draw_networkx(G, pos=positions, labels=label, font_size=6)
+nx.draw_networkx_edge_labels(G, positions, edge_labels=edge_weights, font_size=6)
+plt.show()
